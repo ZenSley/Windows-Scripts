38a199f6f48035c99c007b19f8b9a31a2e826e9c
ZenSley <146985032+ZenSley@users.noreply.github.com>
2023-10-06 09:02:26 -0400

    Create processes.ps1
    Uses Get-CimInstance to pull processes, PID, Path, CmdLine, PPID, & SHA256 for the executable.

---
+++ Data_Gather/processes.ps1
@@ -0,0 +1,9 @@
+#Uses Get-CimInstance to pull processes, PID, Path, CmdLine, PPID, & SHA256 for the executable.  
+
+$procs = Get-CimInstance -ClassName Win32_Process   |
+    Select-Object -Property ProcessName,
+                            ProcessID,
+                            Path,
+                            CommandLine,
+                            ParentProcessID,
+                            @{n="Hash"; e={(Get-FileHash -Path $_.path).hash}}
