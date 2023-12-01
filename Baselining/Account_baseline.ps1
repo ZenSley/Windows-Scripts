d53dfd364cca3f1c289102a4af0a328bc69fcd55
ZenSley <146985032+ZenSley@users.noreply.github.com>
2023-12-01 18:25:02 -0500

    Update Account_baseline.ps1

--- Baselining/Account_baseline.ps1
+++ Baselining/Account_baseline.ps1
@@ -1,4 +1,4 @@
 $Creds = Get-Credentials
 $Targets = # import target list with target hostnames or IP 
 
-Invoke-Command -CN $targets -CR $Creds -Filepath ..\Data_Gather\Accounts.ps1 | Export-Csv .\accountsBaseline.csv
+Invoke-Command -CN $targets -CR $Creds -FilePath ..\Data_Gather\autoruns.ps1 -ArgumentList (,(Get-Content ..\Data_Gather\Autoruns.txt)) | Export-Csv .\AutoRunsBaseline.csv
