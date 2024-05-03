52df6cc3b96bbe734b67cbd7e8dbbe0b85b9fe5c
ZenSley <146985032+ZenSley@users.noreply.github.com>
2024-05-02 20:53:58 -0400

    Update services.ps1

--- Data_Gather/services.ps1
+++ Data_Gather/services.ps1
@@ -1,9 +1,33 @@
-$ServiceStat = ( Get-CimInstance -ClassName Win32_Service |
-    Select-Object -Property @{n="ServiceName";e={$_.name}},
-                            @{n="Status";e={$_.state}},
-                            @{n="StartType";e={$_.startmode}},
-                            PathName 
-                            )
+$winservices = Get-Service | Select-Object *
+$CIMServices = Get-CimInstance -ClassName Win32_Service | Select-Object -Property * 
 
-$ServiceStat | Format-Table -Wrap 
+$svcData = @(foreach($winservice in $winservices) {
+    $imagepath = get-itempropertyvalue -path "HKLM:\SYSTEM\CurrentControlSet\Services\$($winservice.Name)\" -name ImagePath -ErrorAction SilentlyContinue
+    $servicedll = get-itempropertyvalue -path "HKLM:\SYSTEM\CurrentControlSet\Services\$($winservice.Name)\Parameters" -name ServiceDLL -ErrorAction SilentlyContinue    
+    $DllHash = (Get-FileHash -Path $servicedll -ErrorAction SilentlyContinue).hash
+    $matchedSvc = ($CIMServices | where { $_.name -eq $winservice.servicename}) 
 
+    $dict=@{
+            "service.name" = $winservice.servicename    
+            "requiredservices"= $winservice.requiredservices    
+            "canpauseandcontinue" = $winservice.canpauseandcontinue    
+            "canshutdown" = $winservice.canshutdown    
+            "canstop" = $winservice.canstop    
+            "displayname" = $winservice.displayname    
+            "dependentservices" = $winservice.dependentservices    
+            "servicedependedon" = $winservice.servicesdependedon    
+            "servicehandle" = $winservice.servicehandle    
+            "service.state" = $winservice.status    
+            "service.type" = $winservice.servicetype    
+            "starttype" = $winservice.starttype   
+            "imagepath" = $imagepath   
+            "servicedll" = $servicedll
+            "servicedll.hash" = $dllHash 
+            "service.ProcessID" = $matchedSvc.ProcessID
+            "service.description" = $matchedSvc.Description  
+            "service.installdate" = $matchedSvc.installdate
+
+            }   
+    
+    New-Object -TypeName psobject -Property $dict | sort -Property Service.Name 
+    })
