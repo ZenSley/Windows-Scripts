979adc83feda5759f1be32641eae2c6ab1c9d7f5
ZenSley <146985032+ZenSley@users.noreply.github.com>
2024-05-20 14:36:17 -0400

    Update get_schtasks.ps1

--- Data_Gather/get_schtasks.ps1
+++ Data_Gather/get_schtasks.ps1
@@ -17,23 +17,33 @@ $formattedTasks = @(foreach($task in $schtasksObj){
         }elseif ($ExePath -like "*%programfiles(x86)%*") {
         $cleanExePath = $ExePath.replace('%programfiles(x86)%', ${env:ProgramFiles(x86)})
         }elseif ([string]::IsNullOrEmpty($ExePath)) {
-        $cleanExePath = 'No Executable Provide'
+        $cleanExePath = 'No Executable Provided'
         }else{
         $cleanExePath = $ExePath
         }
     $data = @{
-                TaskName = $SchTaskMatched.taskname
-                Status = $task.Status
-                Description = $SchTaskMatched.Description
-                Run_As_User = $task.'Run As User'
-                Next_Run_Time = $task.'Next Run Time'   
-                Last_Run_Time = $task.'Last Run Time'
-                Start_Time = $task.'Start Time'
-                End_Date = $task.'End Date'
-                Task_to_Run = $task.'Task To Run'
-                Exe_Hash = (Get-FileHash $cleanExePath -ErrorAction SilentlyContinue).hash
+                'Task.Name' = $SchTaskMatched.taskname
+                'Task.Path' = $SchTaskMatched.TaskPath
+                'Task.Status' = $task.Status
+                'Task.State' = $task.'Scheduled Task State'
+                'Task.Author' = $task.Author 
+                'Task.Description' = $SchTaskMatched.Description
+                'Task.TriggerType' = $task.'Schedule Type'
+                'Task.RepeatInterval' = $task.'Repeat: Every'
+                'Task.Run_As_User' = $task.'Run As User'
+                'Task.Next_Run_Time' = $task.'Next Run Time'   
+                'Task.Last_Run_Time' = $task.'Last Run Time'
+                'Task.StartTime' = $task.'Start Time'
+                'Task.EndDate' = $task.'End Date'
+                'Task.Task_to_Run' = $task.'Task To Run'
+                'Task.ExecutableSHA256' = (Get-FileHash $cleanExePath -Algorithm SHA256 -ErrorAction SilentlyContinue).hash
+                'Task.ExecutableSHA1' = (Get-FileHash $cleanExePath -Algorithm SHA1 -ErrorAction SilentlyContinue).hash
+                'Task.ExecutableMD5' = (Get-FileHash $cleanExePath -Algorithm MD5 -ErrorAction SilentlyContinue).hash
+                
     } 
     New-Object -TypeName psobject -Property $data | sort -Property TaskName
 }) 
 
-$formattedTasks | Sort-Object -Property TaskName 
+#$formattedTasks | Sort-Object -Property TaskName 
+
+
